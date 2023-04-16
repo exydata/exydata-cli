629aa7928ef804ae15a0f1e83e18f76a85da7723
Ignacio Van Droogenbroeck <ignacio@vandroogenbroeck.net>
2023-04-16 17:03:35 -0500

    added a pyproject.toml file

---
+++ install.sh
@@ -0,0 +1,13 @@
+# download voltmetrix-cli from github and install it to /usr/local/bin
+
+# download voltmetrix-cli
+
+wget https://github.com/volmetrixdotcom/cli/releases/download/0.0.1/voltmetrix -O /usr/local/bin/voltmetrix
+
+# make it executable
+
+chmod +x /usr/local/bin/voltmetrix
+
+# done
+
+echo "voltmetrix-cli installed to /usr/local/bin"
